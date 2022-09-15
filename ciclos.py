afd653c2d9d47492afa815ed572471f062ed05d1
Antony Botello <antonybotello@gmail.com>
2022-09-14 20:56:02 -0500

    crud

--- ciclos.py
+++ ciclos.py
@@ -13,3 +13,4 @@ print("Se ha inicado el carrito. En total hay 0 manzanas.")
 for i in range(1,6):
     print(f"Se ha agregado una manzana a la canasta. Ahora hay {i} manzanas.")
 
+
---
+++ crud.py
@@ -0,0 +1,12 @@
+# menú principal
+opcion = None
+
+while opcion != 5:
+    print("MENÚ PRINCIPAL PRODUCTOS")
+    print("1. Crear producto")
+    print("2. Ver producto")
+    print("3. Actualizar producto")
+    print("4. Eliminar producto")
+    print("5. Salir")
+
+    opcion = int(input("Digite la opcion deseada:\n"))
