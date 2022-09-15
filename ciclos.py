c5e0e786dbe5d40b7c0eacd5f740b870c4ee7e47
Antony Botello <antonybotello@gmail.com>
2022-09-14 20:36:04 -0500

    ciclos

---
+++ ciclos.py
@@ -0,0 +1,15 @@
+# Ciclo iterativo while controlado por condiciones
+"""
+manzanas = 20
+contador=0
+print(f"Se ha inicado el carrito. En total hay {contador} manzanas.")
+
+while(contador < manzanas):
+    contador+=1
+    print(f"Se ha agregado una manzana a la canasta. Ahora hay {contador} manzanas.")
+"""
+# Ciclo iterativo for controlado por cantidad
+print("Se ha inicado el carrito. En total hay 0 manzanas.")
+for i in range(1,6):
+    print(f"Se ha agregado una manzana a la canasta. Ahora hay {i} manzanas.")
+
---
+++ variables.py
@@ -0,0 +1,12 @@
+# Banderas
+suma_realizada = False
+total = 0
+a = 5
+b = 10
+
+if (suma_realizada == False):
+    total = a + b
+    suma_realizada= True
+
+if (suma_realizada == True):
+    print("Se ha realizado una suma y su valor es", total)
