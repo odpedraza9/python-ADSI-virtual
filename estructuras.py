8dbf2d02d4eb4069180512ddd7b26c05373c4e17
odpedraza9 <110872198+odpedraza9@users.noreply.github.com>
2022-09-14 20:01:50 -0500

    Update estructuras.py

--- estructuras.py
+++ estructuras.py
@@ -21,3 +21,24 @@ if 0 < x and x < 10:
 
 if 0 < x < 10:
     print("x es un numero positivo de un solo dígito.")  # recomendable
+
+
+
+# ejercicio  
+El profesor le indica a Diego que durante el curso se sacaron 4 notas y todas tienen el mismo valor. Además le
+solicita que debe elaborar un programa que le ayude a calcular si aprobó o debe recuperar la materia no solo a él
+sino a todos sus compañeros.
+Para esto debe planificar correctamente un código en python que les ayude a automatizar este proceso.
+
+
+nota1= 4.5
+nota2= 2.9
+nota3= 3.8
+Nota4= 5.0
+
+promedio= round((nota1+nota2+nota3+Nota4)/4.1)
+
+if promedio >=3:
+    print(f"su nota fue de {promedio}, usted Aprobo")
+else:
+    print(f"su nota fue de {promedio}, usted reprobo")
